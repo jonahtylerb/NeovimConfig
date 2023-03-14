f469c8ee0b0f4f7f2152bff7545fb93ba069f0f6
Jonah Brupbacher <jonahtylerb@gmail.com>
2023-03-13 22:46:41 -0500

    =Rewrote config using LazyVim

---
+++ lua/plugins/init.lua
@@ -0,0 +1,257 @@
+require("nvim-treesitter.install").prefer_git = false
+require("nvim-treesitter.install").compilers = { "clang" }
+
+return {
+  {
+    "marko-cerovac/material.nvim",
+    opts = {
+      lualine_style = "stealth",
+      contrast = {
+        terminal = true,
+        sidebars = true,
+        floating_windows = true,
+        cursor_line = true,
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "material",
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "html",
+        "svelte",
+        "astro",
+        "javascript",
+        "css",
+        "json",
+        "markdown",
+        "markdown_inline",
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "svelte-language-server",
+        "shellcheck",
+        "astro-language-server",
+        "css-lsp",
+        "emmet-ls",
+        "html-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "prettierd",
+        "shfmt",
+        "stylua",
+        "typescript-language-server",
+        "unocss-language-server",
+      },
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        animation = require("mini.indentscope").gen_animation.linear({ duration = 30, unit = "total" }),
+      },
+    },
+  },
+
+  {
+    "cljoly/telescope-repo.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("telescope").load_extension("repo")
+    end,
+  },
+
+  {
+    "mg979/vim-visual-multi",
+    event = "BufNewFile",
+  },
+
+  {
+    "tenxsoydev/karen-yank.nvim",
+    event = "BufNewFile",
+    opts = {},
+  },
+
+  {
+    "ggandor/leap-spooky.nvim",
+    event = "BufNewFile",
+    opts = {
+      paste_on_remote_yank = true,
+    },
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- they way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+
+  {
+    "echasnovski/mini.starter",
+    version = false,
+    event = "VimEnter",
+    opts = function()
+      local logo =
+        "                                                 _.oo.          \n                         _.u[[/;:,.         .odMMMMMM'          \n                      .o888UU[[[/;:-.  .o@P^    MMM^            \n                     oN88888UU[[[/;::-.        dP^              \n███╗   ██╗███████╗  dNMMNN888UU[[[/;:--. ██╗.@P██╗██╗███╗   ███╗\n████╗  ██║██╔════╝ ,MMMMMMN888UU[[/;::-. ██║   ██║██║████╗ ████║\n██╔██╗ ██║█████╗   NNMMMNN888UU[[[/~.o@P^██║   ██║██║██╔████╔██║\n██║╚██╗██║██╔══╝   888888888UU[[[/o@^-.. ╚██╗ ██╔╝██║██║╚██╔╝██║\n██║ ╚████║███████╗oI8888UU[[[/o@P^:--..   ╚████╔╝ ██║██║ ╚═╝ ██║\n╚═╝  ╚═══╝╚══════╝  YUU[[[/o@^;::---..     ╚═══╝  ╚═╝╚═╝     ╚═╝\n             oMP     ^/o@P^;:::---..                            \n          .dMMM    .o@^ ^;::---...                              \n         dMMMMMMM@^`       `^^^^                                \n        YMMMUP^                                                 \n         ^^                                                     "
+      local pad = string.rep(" ", 22)
+      local new_section = function(name, action, section)
+        return { name = name, action = action, section = pad .. section }
+      end
+
+      local starter = require("mini.starter")
+    --stylua: ignore
+    local config = {
+      evaluate_single = true,
+      header = logo,
+      items = {
+        new_section("Find file",    "Telescope find_files", "Telescope"),
+        new_section("Recent files", "Telescope oldfiles",   "Telescope"),
+        new_section("Grep text",    "Telescope live_grep",  "Telescope"),
+        new_section("init.lua",     "e $MYVIMRC",           "Config"),
+        new_section("Lazy",         "Lazy",                 "Config"),
+        new_section("New file",     "ene | startinsert",    "Built-in"),
+        new_section("Quit",         "qa",                   "Built-in"),
+      },
+      content_hooks = {
+        starter.gen_hook.adding_bullet(pad .. "░ ", false),
+        starter.gen_hook.aligning("center", "center"),
+      },
+    }
+      return config
+    end,
+    config = function(_, config)
+      -- close Lazy and re-open when starter is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "MiniStarterOpened",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      local starter = require("mini.starter")
+      starter.setup(config)
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          local pad_footer = string.rep(" ", 8)
+          starter.config.footer = pad_footer
+            .. "⚡ Neovim loaded "
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms\n                 Whoa that Vim is fast!"
+          pcall(starter.refresh)
+        end,
+      })
+    end,
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        separator_style = "slant",
+      },
+    },
+  },
+
+  {
+    "gen740/SmoothCursor.nvim",
+    event = "BufNewFile",
+    opts = {
+      autostart = true,
+      cursor = "",
+      texthl = "SmoothCursor",
+      linehl = "Cursorline",
+      type = "default",
+      fancy = {
+        enable = true,
+        head = { cursor = "▷", texthl = "SmoothCursor", linehl = nil },
+        body = {
+          { cursor = "", texthl = "SmoothCursorRed" },
+          { cursor = "•", texthl = "SmoothCursorOrange" },
+          { cursor = "•", texthl = "SmoothCursorOrange" },
+          { cursor = ".", texthl = "SmoothCursorYellow" },
+        },
+        tail = { cursor = nil, texthl = "SmoothCursor" },
+      },
+      speed = 50,
+    },
+  },
+
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.prettierd)
+    end,
+  },
+}
